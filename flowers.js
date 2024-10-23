0baeba4c04fe616e7515bfbc7684c0ff56f36518
katyasolis <149627423+katyasolis@users.noreply.github.com>
2024-10-23 15:24:31 -0500

    flower function works again and audio functions moved to sketch.js only

--- flowers.js
+++ flowers.js
@@ -1,34 +1,3 @@
-let midColor1, petalColor1;
-//let mic, amplitude;
-
-function setup() {
-    createCanvas(400, 400);
-    noLoop();
-
-    //flower colors
-    midColor1 = color(50, 70, 100);
-    petalColor1 = color(300, 80, 100);
-
-    //Audio setup
-    mic = new p5.AudioIn();
-    mic.start();
-
-    amplitude = new p5.Amplitude();
-    amplitude.setInput(mic);
-
-    
-}
-
-function draw() {
-
-    flower(200, 200 + moveY, 6, midColor1, petalColor1); 
-    //flower(300, 300 + moveY, 5, midColor1, petalColor1);
-
-    let level = amplitude.getLevel();
-
-    let moveY = map(level, 0, 1, -50, 50);    
-}
-
 function flower(x, y, numPetals, midColor, petalColor) {
     // Drawing petals
     for (let i = 0; i < numPetals; i++) {
@@ -48,6 +17,7 @@ function flower(x, y, numPetals, midColor, petalColor) {
 
 function drawPetal(petalColor) {
     fill(petalColor);
+    stroke(0); 
 
     beginShape();
     curveVertex(0, 0); // Control point
@@ -57,6 +27,7 @@ function drawPetal(petalColor) {
     curveVertex(0, 0); // Control point
     endShape(CLOSE);
 }
+
 
 // Testing
 function mousePressed() {
--- sketch.js
+++ sketch.js
@@ -1,28 +1,40 @@
 let ratio = 1.6;
 let globeScale;
+
 let mic, amplitude;
 
+let midColor1, petalColor1, petalColor2;
+
 function setup() {
-    createCanvas(window.innerWidth, window.innerWidth/ratio);
+    createCanvas(window.innerWidth, window.innerWidth / ratio);
+    noLoop();
     globeScale = min(width, height);
     colorMode(HSB);
+    //getAudioContext().suspend();
 
-    mic = new p5.AudioIn();
+    //flower colors
+    midColor1 = color(50, 70, 100);
+    petalColor1 = color(300, 80, 100);
+    petalColor2 = color(200, 80, 100);
+
+   /* mic = new p5.AudioIn();
     mic.start();
 
     amplitude = new p5.Amplitude();
-    amplitude.setInput(mic);
+    amplitude.setInput(mic);*/
 }
 
 function draw() {
     background(210, 35, 100); 
 
-    let level = amplitude.getLevel();
 
-    let moveY = map(level, 0, 1, -50, 50)
+    /*let level = amplitude.getLevel();
 
-    flower(width / 2, height / 2 + moveY);
+    let moveY = map(level, 0, 1, -50, 50)*/
 
-    rainDrop();
+    flower(200, 200, 6, midColor1, petalColor1);
+    flower(400, 200, 6, midColor1, petalColor2);
+
+    //rainDrop();
  
 }
