c1b9b77e030237ed552c4f13330c94737b528808
katyasolis <149627423+katyasolis@users.noreply.github.com>
2024-10-23 15:49:06 -0500

    audio slider in and some work on audio but still not working

--- sketch.js
+++ sketch.js
@@ -1,7 +1,10 @@
 let ratio = 1.6;
 let globeScale;
 
-let mic, amplitude;
+let mic, amplitude, startAudio, volSenseSlider;
+let vol = 1;
+let volSense = 100;
+let slideStep = 10;
 
 let midColor1, petalColor1, petalColor2;
 
@@ -10,31 +13,55 @@ function setup() {
     noLoop();
     globeScale = min(width, height);
     colorMode(HSB);
-    //getAudioContext().suspend();
+    getAudioContext().suspend();
 
     //flower colors
     midColor1 = color(50, 70, 100);
     petalColor1 = color(300, 80, 100);
     petalColor2 = color(200, 80, 100);
 
-   /* mic = new p5.AudioIn();
+    //Audio Setup
+    mic = new p5.AudioIn();
     mic.start();
 
     amplitude = new p5.Amplitude();
-    amplitude.setInput(mic);*/
+    amplitude.setInput(mic);
+
+    volSenseSlider = createSlider(0, 200, volSense, slideStep);
 }
 
 function draw() {
     background(210, 35, 100); 
 
+    if (startAudio) {
+        vol = mic.getLevel();
 
-    /*let level = amplitude.getLevel();
+        volSense = volSenseSlider.value();
+        normVol = vol * volSense;
 
-    let moveY = map(level, 0, 1, -50, 50)*/
+        Amplitude();
+    }
+
 
     flower(200, 200, 6, midColor1, petalColor1);
     flower(400, 200, 6, midColor1, petalColor2);
 
     //rainDrop();
  
+}
+
+function mousePressed() {
+    getAudioContext().resume();
+    if(!startAudio){
+        mic = new p5.AudioIn();
+
+        mic.start();
+        startAudio = true;
+    }
+}
+
+function Amplitude() {
+    let level = amplitude.getLevel();
+
+    let moveY = map(level, 0, 1, -50, 50);
 }
