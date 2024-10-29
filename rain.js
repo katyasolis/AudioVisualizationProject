09927f3d80088d792151a67d540c01e1ec84f23c
katyasolis <149627423+katyasolis@users.noreply.github.com>
2024-10-29 10:06:14 -0500

    Combined branches with rain and flower updates

--- rain.js
+++ rain.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 function rainDrop(x, y, width, height) {
     noFill();
     fill(210, 100, 100); // Set fill color
@@ -9,5 +10,34 @@ function rainDrop(x, y, width, height) {
     vertex(x, y);
     bezierVertex(x - width / 2, y + height / 4, x - width / 2, y + height, x, y + height);
     bezierVertex(x + width, y + height, x + width / 2, y + height / 4, x, y);    endShape(CLOSE);
+=======
+class Raindrop {
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+>>>>>>> Stashed changes
     }
-    
+
+    update(yOffset) {
+        this.y += yOffset;
+        if (this.y > height) {
+            this.y = 0; // Reset to the top if it goes off the bottom
+        }
+    }
+
+    display() {
+        noFill();
+        fill(210, 100, 100); // Set fill color
+        noStroke();
+        stroke('black'); // Set stroke color
+        strokeWeight(2); // Set stroke weight
+
+        beginShape();
+        vertex(this.x, this.y);
+        bezierVertex(this.x - this.width / 2, this.y + this.height / 4, this.x - this.width / 2, this.y + this.height, this.x, this.y + this.height);
+        bezierVertex(this.x + this.width, this.y + this.height, this.x + this.width / 2, this.y + this.height / 4, this.x, this.y);
+        endShape(CLOSE);
+    }
+}
--- sketch.js
+++ sketch.js
@@ -10,6 +10,9 @@ let startAudio = false;
 
 let midColor1, petalColor1, petalColor2;
 
+let raindrops = [];
+let numRaindrops = 50;
+
 function setup() {
     createCanvas(window.innerWidth, window.innerWidth / ratio);
     globeScale = min(width, height);
@@ -28,12 +31,18 @@ function setup() {
     mic.start();
     fft = new p5.FFT();
     fft.setInput(mic);
+
+    for (let i = 0; i < numRaindrops; i++) {
+        raindrops.push(new Raindrop(random(width), random(height), 20, 40));
+    }
 }
 
 function draw() {
     background(210, 35, 100); 
 
     let yOffset = 0;
+
+    let rainDropyOffset = 0;
 
     if (startAudio) {
         vol = mic.getLevel();
@@ -43,13 +52,49 @@ function draw() {
         volSense = volSenseSlider.value();
         normVol = vol * volSense;
 
+<<<<<<< Updated upstream
         yOffset = map(normVol, 0, 1, 0, -100);
+=======
+        yOffset = map(normVol, 0, 1, 0, -50) * .375;
+        yOffset2 = map(normVol, 0, 1, 0, -50) * .475;
+        yOffset3 = map(normVol, 0, 1, 0, -50) * .575;
+
+        //Constraint flowers to not go past the top of the screen
+        yOffset = constrain(yOffset, -200, 0);
+        yOffset2 = constrain(yOffset2, -200, 0);
+        yOffset3 = constrain(yOffset3, -200, 0);
+
+        baseHue1 = hue(petalColor1);
+        baseHue2 = hue(petalColor2);
+        baseHue3 = hue(petalColor3);
+
+        // Adjust the hue value based on the sound input
+        baseHue1 = (baseHue1 + map(normVol, 0, 1, -10, 10)) % 50;
+        baseHue2 = (baseHue2 + map(normVol, 0, 1, -10, 10)) % 50;
+        baseHue3 = (baseHue3 + map(normVol, 0, 1, -10, 10)) % 50;
+
+        // Set the petal colors based on the base hue value
+        /*petalColor1 = color(baseHue1, 100, 100);
+        petalColor2 = color(baseHue2, 100, 100);
+        petalColor3 = color(baseHue3, 100, 100);*/
+
+        rainDropyOffset = map(normVol, 0, 1, 0, 5);
+>>>>>>> Stashed changes
     }
 
     flower(200, 200 + yOffset, 6, midColor1, petalColor1);
     flower(400, 200 + yOffset, 6, midColor1, petalColor2); 
 
+<<<<<<< Updated upstream
     rainDrop(width / 2, 0, 20, 40);
+=======
+    for (let raindrop of raindrops) {
+        raindrop.update(rainDropyOffset);
+        raindrop.display();
+    }
+    
+
+>>>>>>> Stashed changes
 }
 
 function mousePressed() {
